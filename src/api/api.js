8f31692731478af1df6c40f2e1cd451db630c1b3
gitaufar <zhafiraufar123@gmail.com>
2025-06-01 16:14:51 +0700

    change to public api key v3

--- src/api/api.js
+++ src/api/api.js
@@ -1,14 +1,12 @@
-const Api_Key = import.meta.env.VITE_TMDB_API_KEY;
-const accessToken = import.meta.env.VITE_TMDB_ACCESS_TOKEN;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const BASE_URL = "https://api.themoviedb.org/3";
 export const BASE_URL_IMAGE = "https://image.tmdb.org/t/p/w500";
 
 export const getNowPlayingMovies = async (page = 1) => {
   try {
-    const res = await fetch(`${BASE_URL}/movie/now_playing?page=${page}`, {
+    const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}&page=${page}`, {
       headers: {
         accept: 'application/json',
-        Authorization: accessToken
       }
     });
 
@@ -16,43 +14,39 @@ export const getNowPlayingMovies = async (page = 1) => {
 
     return res.json();
   } catch (err) {
-    throw err
+    throw err;
   }
-
 };
 
 export const getGenres = async (language) => {
   try {
-    const res = await fetch(`${BASE_URL}/genre/movie/list?language=${language}`, {
+    const res = await fetch(`${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=${language}`, {
       headers: {
         accept: 'application/json',
-        Authorization: accessToken
       }
     });
 
-    if(!res.ok) throw new Error(res.statusText)
+    if (!res.ok) throw new Error(res.statusText);
 
     return res.json();
   } catch (err) {
-    throw err
+    throw err;
   }
-}
+};
 
 export const searchMovies = async (query) => {
   try {
-    const res = await fetch(`${BASE_URL}/search/movie?query=${query}`, {
+    const res = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`, {
       headers: {
         accept: 'application/json',
-        Authorization: accessToken
       }
-    })
+    });
 
-    if (!res.ok) throw new Error(res.statusText)
+    if (!res.ok) throw new Error(res.statusText);
 
     return res.json();
   } catch (err) {
-    console.log(err)
-    throw err
+    console.log(err);
+    throw err;
   }
-
-}
+};
